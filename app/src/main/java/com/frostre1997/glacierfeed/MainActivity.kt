be9aec6f4db49df29d556f3c8c39c6a5dfc8717d
Frostre_ <n9043395@gmail.com>
2026-07-02 21:53:00 +0200

    Create MainActivity with RecyclerView setup
    #14 - added the Initialize MainActivity with RecyclerView and dummy data.

---
+++ app/src/main/java/com/frostre1997/glacierfeed/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.frostre1997.glacierfeed
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Find the RecyclerView by ID
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+        
+        // Set the LayoutManager
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        // Create dummy data for testing
+        val dummyData = listOf(
+            "Welcome to GlacierFeed",
+            "This is a test post",
+            "Minimalist Reddit Client",
+            "Android development from scratch"
+        )
+
+        // Attach the adapter to the RecyclerView
+        recyclerView.adapter = PostAdapter(dummyData)
+    }
+}
+

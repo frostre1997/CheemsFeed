283494ebc50b6e13fe11945fc241bca234123de1
Frostre_ <n9043395@gmail.com>
2026-07-03 15:15:36 +0200

    Implement RedditResponse and related data classes
    Added data classes for Reddit API response handling.

--- app/src/main/java/com/frostre1997/cheemsfeed/RedditPost.kt
+++ app/src/main/java/com/frostre1997/cheemsfeed/RedditPost.kt
@@ -1,7 +1,21 @@
 package com.frostre1997.cheemsfeed
 
-data class RedditPost(
-    val title: String,
-    val author: String,
-    val thumbnail: String?
+import com.google.gson.annotations.SerializedName
+
+data class RedditResponse(
+    @SerializedName("data") val data: ListingData
 )
+
+data class ListingData(
+    @SerializedName("children") val children: List<PostContainer>
+)
+
+data class PostContainer(
+    @SerializedName("data") val data: RedditPost
+)
+
+data class RedditPost(
+    @SerializedName("title") val title: String,
+    @SerializedName("author") val author: String
+)
+

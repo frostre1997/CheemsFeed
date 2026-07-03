a8e8cb6071f12a2cd1370076dbe95afd8e7af6a0
Frostre_ <n9043395@gmail.com>
2026-07-03 16:50:19 +0200

    Enhance RetrofitClient with OkHttpClient
    Added OkHttpClient with User-Agent header to RetrofitClient.

--- app/src/main/java/com/frostre1997/cheemsfeed/RetrofitClient.kt
+++ app/src/main/java/com/frostre1997/cheemsfeed/RetrofitClient.kt
@@ -1,21 +1,26 @@
 package com.frostre1997.cheemsfeed
 
-import retrofit2.Call
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.GET
-
-interface RedditApi {
-    @GET("r/cheemsburbger/hot.json")
-    fun getHotPosts(): Call<RedditResponse>
-}
 
 object RetrofitClient {
     private const val BASE_URL = "https://www.reddit.com/"
 
+    // Creiamo il client con l'header necessario per evitare il 403
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor { chain ->
+            val request = chain.request().newBuilder()
+                .addHeader("User-Agent", "android:com.frostre1997.cheemsfeed:v1.0 (by /u/frostre1997)")
+                .build()
+            chain.proceed(request)
+        }
+        .build()
+
     val instance: RedditApi by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(okHttpClient) // Colleghiamo il client configurato
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(RedditApi::class.java)

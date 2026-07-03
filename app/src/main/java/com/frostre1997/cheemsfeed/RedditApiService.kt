edf8293006b95e9cb42322c619606a040c545d2d
Frostre_ <n9043395@gmail.com>
2026-07-03 08:04:05 +0200

    Change return type of getHotPosts to RedditResponse
    #36 - Replaced the any thing in to a RedditResponse

--- app/src/main/java/com/frostre1997/cheemsfeed/RedditApiService.kt
+++ app/src/main/java/com/frostre1997/cheemsfeed/RedditApiService.kt
@@ -1,10 +1,10 @@
 package com.frostre1997.cheemsfeed
 
+import retrofit2.call
 import retrofit2.http.GET
-import retrofit2.Call
 
 interface RedditApiService {
     @GET("r/all/hot.json")
-    fun getHotPosts(): Call<Any>
+    fun getHotPosts(): Call<RedditResponse>
 }
 

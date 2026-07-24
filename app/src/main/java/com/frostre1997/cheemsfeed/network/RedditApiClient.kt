39e5b97ed8b25bb098676cabbae258210a06df4e
Frostre_ <n9043395@gmail.com>
2026-07-24 13:23:05 +0200

    Update RedditApiClient.kt

--- app/src/main/java/com/frostre1997/cheemsfeed/network/RedditApiClient.kt
+++ app/src/main/java/com/frostre1997/cheemsfeed/network/RedditApiClient.kt
@@ -1,5 +1,6 @@
 package com.frostre1997.cheemsfeed.network
 
+import com.frostre1997.cheemsfeed.BuildConfig
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
